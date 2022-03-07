f2a3315da925d4acca2936733be131f4bef602ae
aidian3k <adrian.nowosielski1@gmail.com>
2022-03-07 01:05:53 +0100

    Dodanie algorytmu BFS-algorytm nie dziala zawsze

---
+++ bfs_algorithm.h
@@ -0,0 +1,17 @@
+#ifndef BFSH_ALGORITHM
+#define BFSH_ALGORITHM
+#include <stdlib.h>
+
+typedef struct elem{
+    int numer;
+    struct elem *next;
+}*kolejka;
+
+kolejka dodaj_na_koniec(kolejka head,int wierzcholek);
+kolejka usun_poczatek(kolejka head);
+int sprawdz_czy_odwiedzone(int *odwiedzone,int liczba_wierzcholkow);
+kolejka dodaj_do_kolejki(kolejka head,listV_t wierzcholek,int *odwiedzone);
+int czy_pusta(kolejka head);
+int BFS_ALGORITHM(int numer_wierzcholka,int liczba_wierzcholkow,listV_t *graph);
+void pisz_kolejka(kolejka kol);
+#endif
--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include "graph.h"
+#include "bfs_algorithm.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(int argc, char **argv) {
-	int W, K; //liczba wierszy i kolumn
-	int wymiary[2];
-	long double a, b; // zakres wag krawedzi w generowanym grafie <a, b>
-	listV_t *my_graph = czytaj_graf_z_pliku(fopen(argv[1],"r"),wymiary);
-	W=wymiary[0];
-	K=wymiary[1];	
-	zapisz_graf_do_pliku(my_graph, W, K, "wyjscie");
-	return 0;	
+	int W,K;
+	double a,b;
+	W=atoi(argv[1]);
+	K=atoi(argv[2]);
+	a=0;
+	b=1;
+	listV_t *graph=generujGraf(W,K,a,b);
+	printf("%d",BFS_ALGORITHM(1,W*K,graph));
 }
 
 
