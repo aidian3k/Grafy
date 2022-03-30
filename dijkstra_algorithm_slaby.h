e49549927548c003e86176fb62024e40ba57f994
CezaryS8 <46067911+CezaryS8@users.noreply.github.com>
2022-03-30 19:56:17 +0200

    usuniecie naglowka starej wersji Dijkstry

--- dijkstra_algorithm_slaby.h
+++
@@ -1,6 +0,0 @@
-#ifndef DIJKSTRAH_ALGORITHM
-#define DIJKSTRAH_ALGORITHM
-
-double DIJKSTRA_ALGORITHM(int liczba_wierzcholkow,int wierzcholek1,int wierzcholek2, listV_t *graph);
-int najmniejsza_waga(double *dojscia,int liczba_wierzcholkow,int *przetworzone);
-#endif
